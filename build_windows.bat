dfe9869223c846598225ce46d663a79813bf749e
RexDex <rexdexpl@gmail.com>
2022-05-03 13:26:15 +0200

    Added git user name to build script

--- build_windows.bat
+++ build_windows.bat
@@ -12,4 +12,8 @@ cmake --build . --config Release
 popd
 
 copy .build\bin\Release\onion.exe bin\
+
+git config --global user.email "rexdexpl@gmail.com"
+git config --global user.name "RexDex (CI)"
+
 bin\onion.exe autoRelease
