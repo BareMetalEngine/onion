e93179bb968198d40aa1421e3322e57459a0c391
RexDex <rexdexpl@gmail.com>
2022-05-17 08:15:39 +0200

    Updated script

--- get_onion.bat
+++ get_onion.bat
@@ -4,5 +4,11 @@ SET RUN_DIR=%~dp0
 SET ONION_FILE=%RUN_DIR%onion.exe
 
 PUSHD %RUN_DIR%
-curl curl --silent -z onion.exe -L -O https://github.com/BareMetalEngine/onion_tool/releases/latest/download/onion.exe
+IF EXIST "%ONION_FILE%" (
+	ECHO Updating onion.exe...
+	curl --silent -z onion.exe -L -O https://github.com/BareMetalEngine/onion_tool/releases/latest/download/onion.exe
+) else (
+	ECHO Fetching onion.exe...
+	curl --silent -L -O https://github.com/BareMetalEngine/onion_tool/releases/latest/download/onion.exe
+)
 POPD
